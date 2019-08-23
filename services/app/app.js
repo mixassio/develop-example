e53a9e6380484730a72ca66fb15f68b7544081fe
mixassio <mixail.bondarev@gmail.com>
2019-08-23 16:21:06 +0300

    delete gulp, add nodemon, hot-reload is work

---
+++ services/app/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello, World! It is me!");
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
